de9d1c0ebd2cbaa34e4de40d85741a80d87af8ac
Matt Pritchard <mattpritchard@Deare2s-iMac.fios-router.home>
2016-10-24 12:44:38 -0400

    improves c and adds great stuff from Perry

---
+++ elpa/capture-20130828.944/capture-autoloads.el
@@ -0,0 +1,17 @@
+;;; capture-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("capture-functions.el" "capture-helpers.el"
+;;;;;;  "capture-keys.el" "capture-pkg.el" "capture-ui.el" "capture-vars.el"
+;;;;;;  "capture.el") (22538 15236 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; capture-autoloads.el ends here
