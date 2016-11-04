393b1b9d24f16e024663e894edc993afa51b3a3e
Matt Pritchard <mattpritchard@Deare2s-iMac.fios-router.home>
2016-11-04 13:55:47 -0400

    configures minimap and better mouse scrolling

---
+++ elpa/schrute-20161024.54/schrute-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "schrute" "20161024.54" "Help you remember there is a better way to do something." '((emacs "24.3")) :url "https://bitbucket.org/shackra/dwight-k.-schrute" :keywords '("convenience"))
